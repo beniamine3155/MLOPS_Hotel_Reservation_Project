153b4dfe3cec268c352bc3a519923f1aac8ee7b9
beniamine3155 <beniamine3155@gmail.com>
2025-07-31 18:23:14 +0600

    deploy port change

--- application.py
+++ application.py
@@ -29,6 +29,6 @@ def index():
     return render_template('index.html', prediction=None)
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=5050)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
+ 
 
